6a9d77e37153ef32f1f39bc74e449ba2e114d40f
davask <davask.42@gmail.com>
2016-04-11 18:27:27 +0200

    Update log display

--- dwl-init.sh
+++ dwl-init.sh
@@ -9,10 +9,10 @@ fi
 
 # declare user dir
 export DWL_USER_DIR=/home/${DWL_USER_NAME}
-echo "update env ${DWL_USER_DIR}"
+echo "update DWL_USER_DIR > ${DWL_USER_DIR}"
 # declare tmp dir
 export DWL_USER_DIR_TMP=${DWL_USER_DIR}/tmp
-echo "update env ${DWL_USER_DIR_TMP}"
+echo "update DWL_USER_DIR_TMP > ${DWL_USER_DIR_TMP}"
 test -d ${DWL_USER_DIR_TMP} || mkdir -p ${DWL_USER_DIR_TMP}
 echo "##### END OF ${DWL_USER_NAME} INIT #####";
 echo "";
