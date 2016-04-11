556febdb2f7765eff064b620581cd0ff6fef2951
davask <davask.42@gmail.com>
2016-04-11 17:54:55 +0200

    Update init

--- dwl-init.sh
+++ dwl-init.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-echo ">>>>> INIT ${DWL_USER_NAME} <<<<<";
+echo "##### ${DWL_USER_NAME} INIT #####";
 /tmp/dwl-init-user.sh;
+echo "##### END OF ${DWL_USER_NAME} INIT #####";
+echo "";
 
 chmod -R 700 ${DWL_INIT_DIR}
-echo ">>>>> LIST /TMP INITIALIZATION FILES <<<<<";
+echo "##### LIST OF ${DWL_INIT_DIR} #####";
 ls -lah ${DWL_INIT_DIR} | sort -r;
+echo "##### END OF LIST OF ${DWL_INIT_DIR} #####";
 echo "";
 
 echo "##### START INITIALIZATION #####";
@@ -17,14 +20,13 @@ done;
 echo "##### END OF INITIALIZATION #####";
 echo "";
 
-echo ">>>>> ACCESS WORKDIR ${DWL_USER_DIR} <<<<<";
+echo "> ACCESS WORKDIR ${DWL_USER_DIR}";
 cd ${DWL_USER_DIR}
 
-echo ">>>>> SWITCH TO USER ${DWL_USER_NAME} <<<<<";
+echo "> SWITCH TO USER ${DWL_USER_NAME}";
 su ${DWL_USER_NAME}
-echo "";
 
 if [ "${DWL_INIT}" = "app" ] || [ "${DWL_KEEP_RUNNING}" = "true" ]; then
-    echo ">>>>> KEEP APP ACTIVE <<<<<";
+    echo "> KEEP APP ACTIVE";
     tail -f /dev/null;
 fi
--- user.sh
+++ user.sh
@@ -9,6 +9,8 @@ fi
 
 # declare user dir
 export DWL_USER_DIR=/home/$DWL_USER_NAME
+echo "DWL_USER_DIR > $DWL_USER_DIR"
 # declare tmp dir
 export DWL_TMP_DIR=$DWL_USER_DIR/tmp
+echo "DWL_TMP_DIR > $DWL_TMP_DIR"
 test -d $DWL_TMP_DIR || mkdir -p $DWL_TMP_DIR
