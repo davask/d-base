9ee3c09d6f337c8cae617989f56d38836b7295f9
davask <davask.42@gmail.com>
2016-04-11 18:33:05 +0200

    Update init

--- dwl-init.sh
+++ dwl-init.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-echo "##### ${DWL_USER_NAME} INIT #####";
-if [ "${DWL_USER_NAME}" != "`cut -d: -f1 /etc/passwd | grep ^${DWL_USER_NAME}$`" ]; then
-    adduser --disabled-password --gecos "" ${DWL_USER_NAME};
-   echo "${DWL_USER_NAME} created !";
-else
-   echo "${DWL_USER_NAME} already exists !";
-fi
-
-# declare user dir
-export DWL_USER_DIR=/home/${DWL_USER_NAME}
-echo "update DWL_USER_DIR > ${DWL_USER_DIR}"
-# declare tmp dir
-export DWL_USER_DIR_TMP=${DWL_USER_DIR}/tmp
-echo "update DWL_USER_DIR_TMP > ${DWL_USER_DIR_TMP}"
-test -d ${DWL_USER_DIR_TMP} || mkdir -p ${DWL_USER_DIR_TMP}
-echo "##### END OF ${DWL_USER_NAME} INIT #####";
-echo "";
+. ${PWD}/dwl-init-user.sh
 
 chmod -R 700 ${DWL_INIT_DIR}
 echo "##### LIST OF ${DWL_INIT_DIR} #####";
