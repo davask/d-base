7148712f33583ee7b8dd73a4288fb7ce87edebdb
davask <davask.42@gmail.com>
2016-04-12 17:30:05 +0200

    Update init function

--- functions.sh
+++ functions.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 # generating dir for user folder
-function dwl_func_mkdir () {
+function dwl_func_mkdir() {
 
-    env_ext=`echo ${2} | awk '{print toupper($1)}'`;
-    env_name="DWL_USER_DIR_${env_ext}";
+    if [ "$1" != '' ] && [ "$2" != '' ]; then
 
-    export ${env_name}=${1}/${2}
-    echo "> Update ${env_name}=${!env_name}"
+        env_ext=`echo ${2} | awk '{print toupper($1)}'`;
+        env_name="DWL_USER_DIR_${env_ext}";
 
-    if [ ! -d ${!env_name} ]; then
-        mkdir -p ${!env_name};
-        echo ">> Directory ${!env_name} created";
+        export ${env_name}=${1}/${2}
+        echo "> Update ${env_name}=${!env_name}"
+
+        if [ ! -d ${!env_name} ]; then
+            mkdir -p ${!env_name};
+            echo ">> Directory ${!env_name} created";
+        else
+            echo ">> Directory ${!env_name} already exists";
+        fi
     else
-        echo ">> Directory ${!env_name} already exists";
+        echo "Missing arguments";
     fi
 
 }
