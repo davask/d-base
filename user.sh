f269914f4b232c8f62ad3d81dd8447e7a93b2798
davask <davask.42@gmail.com>
2016-04-12 15:11:23 +0200

    Add log dir

--- user.sh
+++ user.sh
@@ -11,6 +11,7 @@ fi
 # declare user dir
 export DWL_USER_DIR=/home/${DWL_USER_NAME}
 echo "update DWL_USER_DIR > ${DWL_USER_DIR}"
+
 # declare tmp dir
 export DWL_USER_DIR_TMP=${DWL_USER_DIR}/tmp
 echo "update DWL_USER_DIR_TMP > ${DWL_USER_DIR_TMP}"
@@ -19,6 +20,16 @@ if [ ! -d ${DWL_USER_DIR_TMP} ]; then
     echo 'Directory ${DWL_USER_DIR_TMP} created';
 else
     echo "Directory ${DWL_USER_DIR_TMP} already exists";
+fi
+
+# declare tmp log
+export DWL_USER_DIR_LOG=${DWL_USER_DIR}/log
+echo "update DWL_USER_DIR_LOG > ${DWL_USER_DIR_LOG}"
+if [ ! -d ${DWL_USER_DIR_LOG} ]; then
+    mkdir -p ${DWL_USER_DIR_LOG};
+    echo 'Directory ${DWL_USER_DIR_LOG} created';
+else
+    echo "Directory ${DWL_USER_DIR_LOG} already exists";
 fi
 echo "##### END OF ${DWL_USER_NAME} INIT #####";
 echo "";
