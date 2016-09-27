1743bbfcee82d92f386091099fe7f1c0fcb4d8f4
davask <davask.42@gmail.com>
2016-09-27 22:28:16 +0200

    Automatic update u16.04

--- tmp/dwl/init.sh
+++ tmp/dwl/init.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
+. /tmp/dwl/envvar.sh
 . /tmp/dwl/user.sh
 . /tmp/dwl/ssh.sh
 echo ">> Ubuntu initialized";
 
 echo ">> Base initialized";
+
+. /tmp/dwl/permission.sh
+echo ">> permission assigned";
 
 . /tmp/dwl/keeprunning.sh
